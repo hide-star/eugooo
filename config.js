f5fadd0ebb543161edbe3c1a26e30ab820020cea
hide-star <hide-star@users.noreply.github.com>
2026-06-24 17:42:58 +0800

    security: add password protection to admin.html with SHA256

---
+++ config.js
@@ -0,0 +1,9 @@
+// EUGOO 系统配置
+// 修改此文件中的密码哈希后，需重新运行 build_v7_v3.py 生成页面
+
+var EUGOO_CONFIG = {
+  // admin.html 访问密码的 SHA256 哈希值
+  // 原始密码: Jtgy1688@
+  // 在线生成工具: https://emn178.github.io/online-tools/sha256.html
+  adminPasswordHash: '7815403858aba054b1323b337503197034824ccc651a682167f783aa0a405b9c'
+};
